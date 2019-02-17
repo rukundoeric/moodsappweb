ac6b21b1f5d18a2e42ebacd37f26b20d99f4f546
rukundoeric <45217364+rukundoeric@users.noreply.github.com>
2019-02-17 20:43:10 +0200

    [Feature #164031150] seted home page

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require('express');
+const path    =require('path');
 const app = express();
-
-
+app.set('view engine','ejs');
+app.set('views', path.resolve(__dirname, 'ui', 'src'));
+app.get('/',(req, res) => { res.render('index.ejs')});;
 app.listen(3000, () =>console.log("Server is running on post 3000"));
