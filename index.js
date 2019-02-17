a5d75bbf5bc1a4b3f60b52f780c5863d2e9180b9
rukundoeric <45217364+rukundoeric@users.noreply.github.com>
2019-02-17 23:11:33 +0200

    [Feature #164031150] Collect some errors

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const path    =require('path');
 const app = express();
+require('dotenv').config();
+const PORT = process.env.NODE_ENV === 'development' ? process.env.PORT : 3000;
 app.set('view engine','ejs');
 app.set('views', path.resolve(__dirname, 'ui', 'src'));
 app.get('/',(req, res) => { res.render('index.ejs')});;
-app.listen(3000, () =>console.log("Server is running on post 3000"));
+app.listen(PORT, () =>console.log("Server is running on post "+PORT));
