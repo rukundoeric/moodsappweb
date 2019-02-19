ddd3a00a7c8b432e7b33232162a911c03e647569
rukundoeric <45217364+rukundoeric@users.noreply.github.com>
2019-02-19 12:38:36 +0200

    error resolved

--- server/test/apis/test1.spec.js
+++ server/test/apis/test1.spec.js
@@ -2,7 +2,7 @@ const expect    = require("chai").expect;
 const td = require('../../src/controllers/Calculate');
 describe('Sum of two numbers', () => {
     it('should return mult', () => {
-        expect(td.getMult(2,5)).to.equal(1);;
+        expect(td.getMult(2,5)).to.equal(10);;
     })
     it('should return div', () => {
         expect(td.getDiv(100,10)).to.equal(10);;
