70beedc399e2f57427aa6bdc0d3558fbc8deb418
rukundoeric <45217364+rukundoeric@users.noreply.github.com>
2019-02-19 02:51:59 +0200

    changes on start file

--- start.js
+++ start.js
@@ -2,7 +2,7 @@ import  express from'express';
 import path   from 'path';
 const app = express();
 require('dotenv').config();
-const PORT =3000;
+const PORT =process.env.PORT || 3000;
 app.set('view engine','ejs');
 app.set('views', path.resolve(__dirname, 'ui', 'src'));
 app.get('/',(req, res) => { res.render('index.ejs')});;
